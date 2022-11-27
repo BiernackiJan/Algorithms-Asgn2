0a61cb37202cc4bee088a01f6c8814d14d7e2bf4
Jan <janbier2000@gmail.com>
2022-11-27 04:42:32 +0000

    GUI tests

--- src/main/java/com/algo/asgn2/RecipeApplication.java
+++ src/main/java/com/algo/asgn2/RecipeApplication.java
@@ -2,19 +2,39 @@ package com.algo.asgn2;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class RecipeApplication extends Application {
+
+    private double x, y;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(RecipeApplication.class.getResource("recipe-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        Parent root = FXMLLoader.load(getClass().getResource("recipe.fxml"));
+        stage.setScene(new Scene(root));
+        stage.initStyle(StageStyle.UNDECORATED);
+
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+
+        });
         stage.show();
+
+//        FXMLLoader fxmlLoader = new FXMLLoader(RecipeApplication.class.getResource("recipe-view.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+//        stage.setTitle("Hello!");
+//        stage.setScene(scene);
+//        stage.show();
     }
 
     public static void main(String[] args) {
