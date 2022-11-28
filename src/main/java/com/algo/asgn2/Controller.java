022e5021a040e902f4919955a9e952295b42e42e
Jan <janbier2000@gmail.com>
2022-11-28 22:42:34 +0000

    Commented all GUI panels and included the code for all ChoiceBoxes, TextFields etc.

--- src/main/java/com/algo/asgn2/Controller.java
+++ src/main/java/com/algo/asgn2/Controller.java
@@ -1,5 +1,7 @@
 package com.algo.asgn2;
 
+import Models.BakedGoods;
+import Models.Ingredient;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -39,84 +41,6 @@ public class Controller {
     private Button btnSignout;
 
 
-
-
-    //Home
-    @FXML
-    private Pane pnlOverview;
-
-
-
-    //Ingredients
-    @FXML
-    private Pane pnlIngredients;
-
-
-
-    //Baked Goods
-    @FXML
-    private Pane pnlBakedGoods;
-    @FXML
-    private TextField goodsName;
-    @FXML
-    private TextField originCt;
-    @FXML
-    private TextArea goodsDesc;
-    @FXML
-    private TextField imageUrl;
-    @FXML
-    private Label youHaveAdded;
-    @FXML
-    private ImageView goodsImage;
-    @FXML
-    private ListView<String> listAddedGood;
-
-
-
-    //Recipes
-    @FXML
-    private Pane pnlRecipes;
-
-
-
-
-    //Baked Goods
-    @FXML
-    public void addGood(ActionEvent actionEvent){
-        youHaveAdded.setVisible(true);
-    }
-
-
-//    @Override
-//    public void initialize(URL location, ResourceBundle resources) {
-//        Node[] nodes = new Node[10];
-//        for (int i = 0; i < nodes.length; i++) {
-//            try {
-//
-//                final int j = i;
-//                nodes[i] = FXMLLoader.load(getClass().getResource("Item.fxml"));
-//
-//                //give the items some effect
-//
-//                nodes[i].setOnMouseEntered(event -> {
-//                    nodes[j].setStyle("-fx-background-color : #0A0E3F");
-//                });
-//                nodes[i].setOnMouseExited(event -> {
-//                    nodes[j].setStyle("-fx-background-color : #02030A");
-//                });
-//                pnItems.getChildren().add(nodes[i]);
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//
-//    }
-
-    @FXML
-    public void chooseIngredient(MouseEvent event){}
-
-
-    //Side Panel
     @FXML
     public void handleClicks(ActionEvent actionEvent) {
         if (actionEvent.getSource() == btnIngredients) {
@@ -154,4 +78,110 @@ public class Controller {
             pnlBakedGoods.setVisible(true);
         }
     }
+
+
+
+
+    //Home
+    @FXML
+    private Pane pnlOverview;//Nothing needed here for now Maybe a save and load button + reset facility if needed
+
+
+
+    //Ingredients
+    @FXML
+    private Pane pnlIngredients; //ingredients pane
+    @FXML
+    private TextField ingredientName;
+    @FXML
+    private TextField ingredientKcal; //kcal
+    @FXML
+    private TextArea ingredientDesc;
+    @FXML
+    private TextField perMeasurememt; //kcal per how much
+
+
+    @FXML
+    private void addIngredient(ActionEvent event){} //button to add an ingredient to the system
+
+
+
+    //Baked Goods
+    @FXML
+    private Pane pnlBakedGoods;
+    @FXML
+    private TextField goodsName;
+    @FXML
+    private TextField originCt;
+    @FXML
+    private TextArea goodsDesc;
+    @FXML
+    private TextField imageUrl;
+    @FXML
+    private Label youHaveAdded;//You Have Added:   "Label"
+    @FXML
+    private ImageView goodsImage;//ImageView to show the image of the added BakedGood
+    @FXML
+    private ListView<String> listAddedGood; //show the added BakedGood bellow the button
+    @FXML
+    public void addGood(ActionEvent actionEvent){
+        youHaveAdded.setVisible(true);
+    } //displays the youHaveAdded label bellow the add button
+
+    //Text fields: goodsName, originCt , ImageUrl , goodsDesc
+    //at the bottom under the button is a image view to show the image from good added called goodsImage
+    //there is also a list view to show the good that was just added called listAddedGood
+
+
+
+
+    //Recipes
+    @FXML
+    private Pane pnlRecipes;
+    @FXML
+    private ChoiceBox<BakedGoods> chooseGood; //choose which baked good to make a recipe for
+    @FXML
+    private ListView<Ingredient> ingredientsList;//to list all ingredients and allow to pick one to add
+    @FXML
+    private TextField ingredientGrams;//measurement of Ingredient to the recipe to calculate grams later
+    @FXML
+    private ListView<Ingredient> addedIngredients; //listView to show the ingredients already added to the recipe
+
+
+    @FXML
+    public void addToRec(ActionEvent action){} //Button to add an ingredient to a recipe
+    @FXML
+    public void chooseIngredient(MouseEvent event){}//Choosing an ingredient from the list by pressing it
+
+
+
+//    @Override
+//    public void initialize(URL location, ResourceBundle resources) {
+//        Node[] nodes = new Node[10];
+//        for (int i = 0; i < nodes.length; i++) {
+//            try {
+//
+//                final int j = i;
+//                nodes[i] = FXMLLoader.load(getClass().getResource("Item.fxml"));
+//
+//                //give the items some effect
+//
+//                nodes[i].setOnMouseEntered(event -> {
+//                    nodes[j].setStyle("-fx-background-color : #0A0E3F");
+//                });
+//                nodes[i].setOnMouseExited(event -> {
+//                    nodes[j].setStyle("-fx-background-color : #02030A");
+//                });
+//                pnItems.getChildren().add(nodes[i]);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//
+//    }
+
+
+
+
+
 }
